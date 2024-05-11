1fd1ca80d9f72899c5f25bfe34fbc7cdac164882
Plamen Andreev <plam4u@me.com>
2024-05-12 00:44:18 +0200

    Minor updates

--- local-install.sh
+++ local-install.sh
@@ -2,7 +2,7 @@
 
 
 if [ -x /opt/homebrew/bin/brew ]; then
+	# export homebrew variables for current script
 	eval $(/opt/homebrew/bin/brew shellenv)
-	echo $HOMEBREW_PREFIX
-	echo $(which brew)
+	brew install --file $HOME/.dotfiles/Brewfile
 fi
