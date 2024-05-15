9b851902c81b189146eec50b2888e9b0a57d48e2
Plamen Andreev <plam4u@me.com>
2024-05-15 09:41:19 +0200

    Minor updates

--- local-install-macos.sh
+++ local-install-macos.sh
@@ -28,4 +28,12 @@ defaults write com.apple.screencapture "target" -string "clipboard"
 defaults write com.apple.dock "autohide" -bool YES
 defaults write com.apple.dock "show-recents" -bool NO
 
+if [ -x dockutil ]; then
+    dockutil -a /System/Applications/Calendar.app/ --no-restart
+    dockutil -a /System/Applications/Reminders.app/ --no-restart
+    dockutil -a /System/Applications/Notes.app/ --no-restart
+    dockutil -a /Applications/Safari.app/ --no-restart
+    dockutil -a /System/Applications/Mail.app/ --restart
+fi
+
 # Safari
