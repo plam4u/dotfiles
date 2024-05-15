ee98bd44d35bbce14eaab94ac2dfcbbc7a314725
Plamen Andreev <plam4u@me.com>
2024-05-15 08:57:12 +0200

    Minor updates

--- local-install-macos.sh
+++ local-install-macos.sh
@@ -4,12 +4,19 @@
 defaults write NSGlobalDomain "AppleShowAllExtensions" -bool YES
 
 # Finder
+defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
+defaults write com.apple.finder "AppleShowAllFiles" -bool YES
 defaults write com.apple.finder "ShowPathbar" -bool YES
 defaults write com.apple.finder "ShowStatusBar" -bool YES
 defaults write com.apple.finder "NSWindowTabbingShoudShowTabBarKey-com.apple.finder.TBrowserWindow" -bool YES
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool YES
+defaults write com.apple.finder "FXRemoveOldTrashItems" -bool NO
 defaults write com.apple.finder "WarnOnEmptyTrash" -bool NO
 defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool NO
 defaults write com.apple.finder "ShowRecentTags" -bool NO
 defaults write com.apple.finder "NewWindowTarget" -string "PfLo"
 defaults write com.apple.finder "NewWindowTargetPath" -string "file:///Users/$USER/Downloads/"
 defaults write com.apple.finder "NSNavLastRootDirectory" -string "~/Downloads"
+
+# Screenshots
+defaults write com.apple.screencapture "show-thumbnail" -bool NO
