4db56eb105bdd006f0388e2e3898f0896e470049
Plamen Andreev <plam4u@me.com>
2024-05-26 10:31:51 +0200

    Minor updates

--- remote-install.sh
+++ remote-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Install brew which will install Command Line Tools for Xcode
+# Install homebrew.
+# This will install also install the required Command Line Tools (CLT) for Xcode.
+# CLT packs git and make binaries.
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Clone dotfiles to ~/.dotfiles
