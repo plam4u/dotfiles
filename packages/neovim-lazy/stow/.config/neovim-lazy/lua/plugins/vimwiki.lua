a0664e803080c4a1af44aaab24290fe39b4e8ee6
Plamen Andreev <plam4u@me.com>
2024-10-06 19:11:17 +0200

    Add plugin vimwiki to neovim-lazy

---
+++ packages/neovim-lazy/stow/.config/neovim-lazy/lua/plugins/vimwiki.lua
@@ -0,0 +1,13 @@
+return {
+  "vimwiki/vimwiki",
+  init = function()
+    -- Configure Vimwiki options
+    vim.g.vimwiki_list = {
+      { path = "~/notes/notes/", syntax = "markdown", ext = ".md" },
+    }
+
+    -- Optional: Set key bindings
+    -- Set <Leader>ww to open the Vimwiki index page
+    vim.api.nvim_set_keymap("n", "<Leader>ww", ":VimwikiIndex<CR>", { noremap = true, silent = true })
+  end,
+}
