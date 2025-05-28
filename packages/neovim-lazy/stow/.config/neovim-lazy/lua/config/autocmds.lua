1e8dd0134d68c62d1e908a6d8faa51b919932e29
Plamen Andreev <plam4u@me.com>
2025-05-28 03:05:13 -0400

    Disable spell check

--- packages/neovim-lazy/stow/.config/neovim-lazy/lua/config/autocmds.lua
+++ packages/neovim-lazy/stow/.config/neovim-lazy/lua/config/autocmds.lua
@@ -2,6 +2,11 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- load local lua settings (e.g. config a python debugger)
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     local project_launch = vim.fn.getcwd() .. "/.nvim/launch.lua"
@@ -11,9 +16,20 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
+-- highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("lazyvim_highlight_yank", { clear = true }),
+  group = augroup("highlight_yank"),
   callback = function()
     (vim.hl or vim.highlight).on_yank({ timeout = 50 }) -- 50 ms, very quick!
+  end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = false
   end,
 })
