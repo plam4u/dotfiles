9d1d696a63825241ee63faf193bbf2f8efc7248e
Plamen Andreev <plam4u@me.com>
2025-05-30 04:18:38 -0400

    Update LazyVim plugins

--- packages/neovim-lazy/stow/.zshrc.d/neovim-lazy.sh
+++ packages/neovim-lazy/stow/.zshrc.d/neovim-lazy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 alias vl="NVIM_APPNAME=neovim-lazy nvim"
+export VLRC="$HOME/.local/share/neovim-lazy/lazy/LazyVim/lua/lazyvim/plugins/init.lua"
+export MYVLRC="$HOME/dotfiles/packages/neovim-lazy/stow/.config/neovim-lazy/lua/config/autocmds.lua"
