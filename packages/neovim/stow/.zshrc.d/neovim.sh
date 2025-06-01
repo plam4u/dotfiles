116a3343095e87e40c867d310ec559e67d55a196
Plamen Andreev <plam4u@me.com>
2025-06-01 17:12:07 -0400

    Alias nvim as v

--- packages/neovim/stow/.zshrc.d/neovim.sh
+++ packages/neovim/stow/.zshrc.d/neovim.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+alias v="nvim"
 # alias vl="NVIM_APPNAME=neovim-lazy nvim"
 # export PATH="$HOME/bin:$PATH"
 export LVRC="$HOME/.local/share/neovim-lazy/lazy/LazyVim/lua/lazyvim/plugins/init.lua"
