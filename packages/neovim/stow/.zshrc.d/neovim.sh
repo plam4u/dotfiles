9a5b6c42809b99e0d29c4e3265287be8937f9e2d
Plamen Andreev <plam4u@me.com>
2025-06-03 12:30:40 -0400

    Add nvim alias "v."

--- packages/neovim/stow/.zshrc.d/neovim.sh
+++ packages/neovim/stow/.zshrc.d/neovim.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 alias v="nvim"
+alias v.="nvim ."
 # alias vl="NVIM_APPNAME=neovim-lazy nvim"
 # export PATH="$HOME/bin:$PATH"
 export LVRC="$HOME/.local/share/neovim-lazy/lazy/LazyVim/lua/lazyvim/plugins/init.lua"
