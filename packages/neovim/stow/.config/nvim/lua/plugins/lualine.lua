4150325741fa2271544af67289859267147d878c
Plamen Andreev <plam4u@me.com>
2025-06-06 10:30:02 -0400

    Add tmux session to the nvim lualine

--- packages/neovim/stow/.config/nvim/lua/plugins/lualine.lua
+++ packages/neovim/stow/.config/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,13 @@ return {
     opts = function(_, opts)
       opts.options.component_separators = "|"
       opts.options.section_separators = ""
+      -- show tmux session name in lualine in section z
+      opts.sections.lualine_z = {
+        function()
+          local tmux_session = vim.fn.system("tmux display-message -p '#W:#S'")
+          return " " .. tmux_session:gsub("%s+", "")
+        end,
+      }
       return opts
     end,
   },
