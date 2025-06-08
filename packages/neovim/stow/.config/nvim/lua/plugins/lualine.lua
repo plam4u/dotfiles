cdcce746b85c4bd2876fcba3a31525daa71cc52f
Plamen Andreev <plam4u@me.com>
2025-06-08 11:28:04 -0400

    Display window-name only in lualine

--- packages/neovim/stow/.config/nvim/lua/plugins/lualine.lua
+++ packages/neovim/stow/.config/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,8 @@ return {
       opts.sections.lualine_z = {
         function()
           if tmux_session == "" and vim.env.TMUX then
-            local result = vim.fn.system("tmux display-message -p '#W:#S' 2>/dev/null")
+            -- local result = vim.fn.system("tmux display-message -p '#W:#S' 2>/dev/null")
+            local result = vim.fn.system("tmux display-message -p '#W' 2>/dev/null")
             if vim.v.shell_error == 0 then
               tmux_session = " " .. result:gsub("%s+", "")
             else
