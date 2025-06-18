cc0138aa3f558d4b67d337f94c8f9ca2f6e8287d
Plamen Andreev <plam4u@me.com>
2025-06-17 21:36:12 -0400

    Disable persistence auto-load

--- packages/neovim/stow/.config/nvim/lua/plugins/vimwiki.lua
+++ packages/neovim/stow/.config/nvim/lua/plugins/vimwiki.lua
@@ -1,58 +1,46 @@
 return {
   {
     "vimwiki/vimwiki",
-    config = function(_, _)
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          -- print("LspAttach autocmd triggered for Vimwiki, bufnr = " .. args.buf)
-          local client = vim.lsp.get_client_by_id(args.data.client_id) or {}
-          local bufnr = args.buf
-          local filetype = vim.bo[bufnr].filetype
-          if filetype == "markdown" and client.name == "marksman" then
-            vim.lsp.buf_detach_client(bufnr, client.id)
-          end
-        end,
-      })
+    init = function()
+      vim.g.vimwiki_list = {
+        {
+          path = "~/notes/notes/",
+          -- diary_rel_path = "../diary/",
+          syntax = "markdown",
+          ext = ".md",
+        },
+      }
     end,
-    -- FileType is triggered too early (before LSP is attached)
-    --   vim.api.nvim_create_autocmd("FileType", {
-    --     pattern = "vimwiki",
-    --     callback = function()
-    --       print("Vimwiki filetype detected, setting up LSP client..., bufnr = " .. vim.api.nvim_get_current_buf())
-    --       local clients = vim.lsp.get_clients()
-    --       for _, client in ipairs(clients) do
-    --         if client.name == "marksman" then
-    --           vim.lsp.buf_detach_client(0, client.id)
-    --           return true
-    --         end
+    keys = {
+      { "<Leader>ww", "<Plug>VimwikiIndex<CR>", desc = "Open Vimwiki Index", silent = true },
+      { "<Leader>w<Leader>w", "<Plug>VimwikiDiaryIndex<CR>", desc = "Open Vimwiki Diary Index", silent = true },
+      { "gl<Space>", "<Plug>VimwikiToggleListItem<CR>", desc = "Toggle Vimwiki List Item", silent = true },
+    },
+    -- config = function(_, _)
+    --   vim.api.nvim_create_autocmd("LspAttach", {
+    --     callback = function(args)
+    --       -- print("LspAttach autocmd triggered for Vimwiki, bufnr = " .. args.buf)
+    --       local client = vim.lsp.get_client_by_id(args.data.client_id) or {}
+    --       local bufnr = args.buf
+    --       local filetype = vim.bo[bufnr].filetype
+    --       if filetype == "markdown" and client.name == "marksman" then
+    --         vim.lsp.buf_detach_client(bufnr, client.id)
     --       end
-    --       return false
     --     end,
     --   })
     -- end,
-    init = function()
-      -- Configure Vimwiki options
-      vim.g.vimwiki_list = {
-        { path = "~/notes/notes/", syntax = "markdown", ext = ".md" },
-      }
-
-      -- Optional: Set key bindings
-      -- Set <Leader>ww to open the Vimwiki index page
-      vim.api.nvim_set_keymap("n", "<Leader>ww", "<Plug>VimwikiIndex<CR>", { noremap = true, silent = true })
-      vim.api.nvim_set_keymap("n", "gl<Space>", "<Plug>VimwikiToggleListItem<CR>", { noremap = true, silent = true })
-    end,
   },
   -- Disable diagnostics for marksman globally
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     servers = {
-  --       marksman = {
-  --         handlers = {
-  --           ["textDocument/publishDiagnostics"] = function() end, -- Disable diagnostics
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        marksman = {
+          handlers = {
+            ["textDocument/publishDiagnostics"] = function() end, -- Disable diagnostics
+          },
+        },
+      },
+    },
+  },
 }
