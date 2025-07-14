32046292b0cfef8e0b8006f2dfdf179aa89d2c9d
Plamen Andreev <plam4u@me.com>
2025-07-14 09:55:39 +0200

    Update qmk install script to latest asdf and qmk config

--- packages/qmk/install.sh
+++ packages/qmk/install.sh
@@ -4,36 +4,42 @@ brew install qmk/qmk/qmk
 
 mkdir -p ~/dev
 if [ ! -d ~/dev/qmk_firmware ]; then
-    echo
-    echo "Cloning plam4u/qmk_firmware into ~/dev/qmk_firmware..."
-    git clone git@github.com:plam4u/qmk_firmware.git ~/dev/qmk_firmware
+  echo
+  echo "Cloning plam4u/qmk_firmware into ~/dev/qmk_firmware..."
+  git clone git@github.com:plam4u/qmk_firmware.git ~/dev/qmk_firmware
 fi
 
 echo
 echo "Running \"qmk setup -H ~/dev/qmk_firmware\"..."
 (
-    cd ~/dev/qmk_firmware
+  cd ~/dev/qmk_firmware
 
-    # upstream is required
-    git remote add upstream https://github.com/qmk/qmk_firmware.git 
+  # upstream is required
+  git remote add upstream https://github.com/qmk/qmk_firmware.git
 
-    # throws an error
-    git rm --cached lib/ugfx &> /dev/null
-    git submodule update --init --recursive
+  # throws an error
+  git rm --cached lib/ugfx &>/dev/null
+  git submodule update --init --recursive
 
-    # export PATH for QMK-installed dependencies needed by QMK setup
-    export PATH="/opt/homebrew/opt/avr-gcc@8/bin:$PATH"
-    export PATH="/opt/homebrew/opt/arm-none-eabi-gcc@8/bin:$PATH"
+  # export PATH for QMK-installed dependencies needed by QMK setup
+  export PATH="/opt/homebrew/opt/avr-gcc@8/bin:$PATH"
+  export PATH="/opt/homebrew/opt/arm-none-eabi-gcc@8/bin:$PATH"
 
-    # -H sets the QMK Home folder since by default it is ~/qmk_firmware
-    qmk setup -H ~/dev/qmk_firmware
+  # -H sets the QMK Home folder since by default it is ~/qmk_firmware
+  qmk setup -H ~/dev/qmk_firmware
 
-    # override local python version used by the author of keymapviz
-    asdf local python latest
-    pip install keymapviz 
+  # override local python version used by the author of keymapviz
+  asdf set python latest
 
-    # compile from anywhere (no need to cd into qml_firmware folder)
-    export QMK_HOME=~/dev/qmk_firmware
+  # install keymapviz to generate keymap layout comments
+  # pip install keymapviz
+
+  # compile from anywhere (no need to cd into qml_firmware folder)
+  export QMK_HOME=~/dev/qmk_firmware
+
+  # set default keyboard and keymap so that `qmk compile` and `qmk flash`
+  # work without additional arguments
+  qmk config user.keymap=plam4u user.keyboard=handwired/dactyl_manuform/5x6
 )
 # Getting Started
 # https://docs.qmk.fm/newbs_getting_started
