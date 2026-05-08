1da5fd0aaca3ebf0eedc9a81f89c55f0c6c2c6b3
Plamen Andreev <plam4u@me.com>
2026-05-08 12:33:04 +0200

    Add heaps package

---
+++ packages/heaps/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+haxelib fixrepo
+haxelib git heaps https://github.com/deepnight/heaps.git
+haxelib install hldx
+haxelib install hlsdl
+haxelib list
---
+++ packages/heaps/uninstall.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+haxelib remove heaps
+haxelib remove hldx
+haxelib remove hlsdl
