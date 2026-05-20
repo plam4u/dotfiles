be0832ab7574189efa896692011c67a025e981cc
Plamen Andreev <plam4u@me.com>
2026-05-20 13:36:26 +0200

    Update vscode extension filenames

--- packages/code/install.sh
+++ packages/code/install.sh
@@ -3,6 +3,6 @@
 pm stow code
 brew install --cask visual-studio-code
 
-for e in base customize godot haxe love2d; do
+for e in vim godot haxe love2d css; do
   cat "extensions/$e" | xargs -L 1 code --install-extension
 done
