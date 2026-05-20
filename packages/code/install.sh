021eb0b83cacae1f6d9ab1f9f3f4882a10802a92
Plamen Andreev <plam4u@me.com>
2026-05-20 10:26:20 +0200

    Update code package installation

--- packages/code/install.sh
+++ packages/code/install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+pm stow code
 brew install --cask visual-studio-code
-cat extensions/extensions-godot | xargs -L 1 code --install-extension
+
+for e in extensions-base extensions-godot extensions-haxe extensions-love2d extensions-others; do
+  cat "extensions/$e" | xargs -L 1 code --install-extension
+done
--- packages/code/uninstall.sh
+++ packages/code/uninstall.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 brew uninstall visual-studio-code
+
+rm -rf ~/.vscode
+rm -rf ~/Library/Application\ Support/Code
+rm -rf ~/Library/Caches/com.microsoft.VSCode
+rm -rf ~/Library/Saved\ Application\ State/com.microsoft.VSCode.savedState
+if [[ -e /opt/homebrew/bin/code ]]; then
+  rm /opt/homebrew/bin/code
+fi
+git clean -fdX "stow/Library/Application Support/Code"
