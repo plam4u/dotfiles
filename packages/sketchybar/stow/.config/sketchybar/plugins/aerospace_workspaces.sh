095c3d1ad74e98e04c01cc7f9187242ee60a96c3
Plamen Andreev <plam4u@me.com>
2026-09-11 12:38:25 +0300

    Update sketchybar config

---
+++ packages/sketchybar/stow/.config/sketchybar/plugins/aerospace_workspaces.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Remove existing workspace items
+for item in $(sketchybar --query bar | jq -r '.items[]' | grep '^space\.'); do
+  sketchybar --remove "$item"
+done
+
+# Recreate them from AeroSpace
+for sid in $(aerospace list-workspaces --all); do
+  sketchybar --add item "space.$sid" left \
+    --set "space.$sid" \
+    icon="$sid" \
+    label.drawing=off \
+    background.color=0x40ffffff \
+    background.corner_radius=5 \
+    background.height=25 \
+    click_script="aerospace workspace $sid" \
+    script="$CONFIG_DIR/plugins/aerospace.sh $sid" \
+    --subscribe "space.$sid" aerospace_workspace_change
+done
