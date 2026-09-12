ffb79f6548f1165bfd0641d38a61ec54f2703262
Plamen Andreev <plam4u@me.com>
2026-09-12 21:49:39 +0300

    Update sketchybar clock format

--- packages/sketchybar/stow/.config/sketchybar/plugins/clock.sh
+++ packages/sketchybar/stow/.config/sketchybar/plugins/clock.sh
@@ -4,4 +4,4 @@
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set "$NAME" label="$(date '+%H:%M | %a %b %d')"
+sketchybar --set "$NAME" label="$(date '+%a %b %d, %H:%M')"
